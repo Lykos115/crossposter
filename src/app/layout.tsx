134c4f28cdd7241a5ed949d7fd7bc9806090e325
Francisco Herrera <javierherrera52@gmail.com>
2023-03-01 15:12:12 -0800

    adding auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import Providers from '../util/providers'
 import './globals.css'
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -6,13 +8,17 @@ export const metadata = {
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+    	<body>
+		<Providers>
+      			{children}
+		</Providers>
+	</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
+'use client'
+
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { useSession, signIn, signOut } from 'next-auth/react'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-  	<div className='bg-black text-white h-screen w-screen justify-center text-center items-center'>
-		Crossposter coming soon ...
-	</div>
-  )
+	const { data:session, status } = useSession()
+
+	if(status === "authenticated") return <div>signed in</div>
+
+	return (
+	<>
+		<div className='flex bg-black text-white h-screen w-screen justify-center text-center items-center'>
+			Crossposter coming soon ...
+		</div>
+		<button onClick={() => signIn()}>sign in</button>
+	</>
+	)
 }
